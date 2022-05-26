9df705e146d84e2b7e384c4deef79770ea044457
4strodev <jumarurex@gmail.com>
2022-05-26 17:51:25 +0200

    Adding static files

--- main.go
+++ main.go
@@ -7,11 +7,18 @@ import (
 )
 
 var port int
+var staticFolder string
 
 func main() {
 	portFlag := flag.Int("port", 80, "Defines the port where the server listen")
+	staticFlag := flag.String("static", "/var/www/silicon_greenhouse/", "Defines where to check for static files to serve the control panel")
 	flag.Parse()
 	port = *portFlag
+	staticFolder = *staticFlag
+
+	fmt.Printf("Static folder: %s\n", staticFolder)
+
+	App.Static("/", staticFolder)
 
 	log.Printf("Listening on port %d\n", port)
 	log.Fatal(App.Listen(fmt.Sprintf(":%d", port)))
